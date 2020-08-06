fcc9220e461f6d754027ffe941884758839c10ba
Devis Evianus <devisevianus7@gmail.com>
2020-08-07 00:23:03 +0700

    initial commit

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Devis Evianus on 17/07/20.
+//  Copyright © 2020 example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
